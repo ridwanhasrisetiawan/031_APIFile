6c1ca3df25e7cc1dadda21beacf3905951cf0319
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-08-10 14:57:24 +0700

    set up Express server with JSON parsing and API routing

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const connectDatabase = require("./config/db");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', require('./routes/api'));
+
+async function startServer() {
+    await connectDatabase();
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();
