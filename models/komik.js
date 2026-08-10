03867ae56c59e337756fb1268fc7f70a7e926c63
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-08-10 15:05:54 +0700

    add komik model to define comic attributes and associations

---
+++ models/komik.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sinopsis: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        gambar: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        penulis_id: {
+            type: DataTypes.INTEGER,    
+        allowNull: false
+        }
+    }, {
+        tableName: 'komik',
+        timestamps: true
+    });
+    Komik.associate = (models) => {
+        Komik.belongsTo(models.Penulis, {
+            foreignKey: 'penulis_id',
+            as: 'penulis'
+        });
+        Komik.belongsToMany(models.Genre, {
+            through: 'komik_genre',
+            foreignKey: 'komik_id',
+            otherKey: 'genre_id',
+            as: 'genres'
+        });
+    };
+    return Komik;
+};
